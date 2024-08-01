e746e876755199f4d1c324f4fe1d3d8551689ebc
anaomart <anaomart@gmail.com>
2024-08-01 03:23:34 +0300

    first commit -first day- set up clerk - the landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { ClerkProvider } from "@clerk/nextjs";
+import {dark} from "@clerk/themes";
+import { ThemeProvider } from './providers/theme-provider'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -15,8 +17,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider  appearance={{ baseTheme: dark }}>
+
     <html lang="en">
+    <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+
+      
       <body className={inter.className}>{children}</body>
+    </ThemeProvider>
     </html>
+    </ClerkProvider>
+
   );
 }
---
+++ src/app/site/layout.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navigation from '../../components/site/navigation'
+
+export default function layout({children}:{children:React.ReactNode}) {
+  return (
+    <main className='h-full'>
+        <Navigation/>
+        {children}</main>
+  )
+}
