3d336f339675e02d4fdf3f8fc0874e5d61121808
anaomart <anaomart@gmail.com>
2024-08-09 08:02:38 +0300

    add

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,17 +1,13 @@
+import { ClerkProvider } from '@clerk/nextjs'
 import React from 'react'
-import Navigation from '../../components/site/navigation'
-import { ClerkProvider } from "@clerk/nextjs";
 import {dark} from "@clerk/themes";
 export default function layout({children}:{children:React.ReactNode}) {
   return (
-
     <ClerkProvider  appearance={{ baseTheme: dark }}>
 
 
-    <main className='h-full'>
-      
-        <Navigation/>
-        {children}</main>
+
+        {children}
     </ClerkProvider>
   )
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,49 @@ import { NextResponse } from "next/server";
 
 export default authMiddleware({
     publicRoutes: ['/site', '/api/uploadthing'],
-   
+    async beforeAuth(auth,req){},
+    async afterAuth(auth,req){
+      const url = req.nextUrl
+      const searchParams = url.searchParams.toString()
+      let hostname = req.headers
+      const pathWithSearchParams = `${url.pathname}${
+        searchParams.length > 0 ? `?${searchParams}` : ''
+      }`
+
+      
+
+      const CutomSubDomain = hostname.get('host')?.split(`${process.env.NEXT_PUBLIC_DOMAIN}`)?.filter(Boolean)[0]
+      console.log(hostname.get('host'))
+      console.log({CutomSubDomain})
+      if(CutomSubDomain){
+        console.log("custom domain")
+        return NextResponse.rewrite(new URL(`/${CutomSubDomain}${pathWithSearchParams}`,req.url))
+      }
+
+      if(url.pathname === '/sign-in' || url.pathname === '/sign-up'){
+        return NextResponse.redirect(new URL('/agency/sign-in',req.url))
+      }
+
+      if(url.pathname === '/' || url.pathname === '/site' && url.host === process.env.NEXT_PUBLIC_DOMAIN){
+        console.log('site')
+        return NextResponse.rewrite(new URL('/site',req.url))
+      }
+
+      
+      if (
+        url.pathname.startsWith('/agency') ||
+        url.pathname.startsWith('/subaccount')
+      ) {
+        return NextResponse.rewrite(new URL(`${pathWithSearchParams}`, req.url))
+      }
+
+      console.log({url,searchParams,hostname,pathWithSearchParams,CutomSubDomain})
+    
+    
+    
+    
+    },
+
   })
 
 export const config = {
